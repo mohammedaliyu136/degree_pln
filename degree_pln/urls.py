f5bc0000e41450179b2d681af4c5f126d0b68b57
mohammedaliyu136 <mohammedaliyu136@gmail.com>
2018-10-29 22:03:20 +0100

    Update urls.py

--- degree_pln/urls.py
+++ degree_pln/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    #url(r'', include('planner.urls')),
+    url(r'', include('planner.urls')),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 ]
